c09625feac9ded15e74b9c5363a7fbfa7e05fb36
alberto.lolli <alberto.lolli@C11-E0TIXZKNYV4>
2022-07-20 10:28:32 +0200

    Prima classe aggiunta da git :)

---
+++ GitTutorial/FirstClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTutorial
+{
+    public class FirstClass
+    {
+        public string Name { get; set; }
+        public int Qta { get; set; }
+    }
+}
